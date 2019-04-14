2ee9c1c55237c1e65eeb0a55f09a7b073bc2b9bf
ramenjuniti <ramenjuniti@gmail.com>
2019-04-14 19:44:59 +0900

    :shower: 無駄な部分削除

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useContext } from "react";
 import { Store } from "./store";
 import TodoList from "./TodoList";
-import Todo from "./Todo";
 import AddTodo from "./AddTodo";
 
 const App = () => {
-  const { state, dispatch } = useContext(Store);
+  const { dispatch } = useContext(Store);
   return (
     <>
       <AddTodo add={text => dispatch({ type: "add", text: text })} />
